73b1636067ca9e9898d18e7ae522fe1581d759d3
Manojkumar886 <maddymano434@gmail.com>
2024-03-12 09:00:32 +0530

    text block is done...!

---
+++ src/main/java/Java_Featurs/textblock.java
@@ -0,0 +1,23 @@
+package Java_Featurs;
+
+public class textblock
+{
+    public static void main(String[] args) {
+        String address="7/149-Rasipuram "+"\n Namakkal,Tamilnadu"+"\n pincode-6372903";
+
+//        text block
+        String htmlcode= """
+                <html>
+                    <head>
+                    </head>
+                    <body>
+                    </body>
+                 </html>
+                """;
+
+        System.out.println(htmlcode==address);//
+
+        System.out.println(address);
+        System.out.println(htmlcode);
+    }
+}
